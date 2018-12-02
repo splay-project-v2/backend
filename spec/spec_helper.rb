74dbd3b45a65fe3a44a4023bb4347ccfd76d3bf5
Samuel Monroe <spat.monroe@gmail.com>
2018-12-02 20:40:59 +0900

    Configured CodeCov for RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,12 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+# Enable CodeCov
+require 'simplecov'
+SimpleCov.start
+
+# Main config
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
