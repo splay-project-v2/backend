1dca508bf74c8677ba754e77770953b0bb066998
Samuel Monroe <spat.monroe@gmail.com>
2018-12-09 18:13:30 +0900

    Model testing on users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
+
+  # Stronger than Devise built-in validator
+  validates :email, format: {
+    with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Creation of user with attributes absence' do
+    it 'is not valid with blank user' do
+      user = User.new
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid with absence of password' do
+      user = User.new(email: 'foo@bar.com')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid with absence of email' do
+      user = User.new(password: 'foobarbaz')
+      expect(user).not_to be_valid
+    end
+  end
+
+  context 'Creation of user with attributes violation' do
+    it 'is not valid with a too short password' do
+      user = User.new(email: 'foo@bar.com', password: '2smal')
+      expect(user).not_to be_valid
+    end
+
+    it 'is not valid with a invalid email address' do
+      user = User.new(email: 'foo@bar', password: 'foobarbaz')
+      expect(user).not_to be_valid
+    end
+  end
+
+  context 'Creation of a valid user' do
+    it 'is valid' do
+      user = User.new(email: 'foo@bar.com', password: 'foobarbaz')
+      expect(user).to be_valid
+    end
+  end
 end
