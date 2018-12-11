c3f85668d8986b8b586a504c8f5c6aab7e2f1cd6
Samuel Monroe <spat.monroe@gmail.com>
2018-12-11 18:16:59 +0900

    Code cleaning

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,10 @@
-class Api::V1::UsersController < ApplicationController
-  def create
-    user = UserService.new(JSON.parse request.body.read).call
-    render json: {'token': user}
+class Api
+  class V1
+    class UsersController < ApplicationController
+      def create
+        user = UserService.new(JSON.parse(request.body.read)).call
+        render json: { 'token': user }
+      end
+    end
   end
 end
