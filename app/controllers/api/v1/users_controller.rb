a4d80fd65a71e764e1407e9090f44f7061caef64
Samuel Monroe <spat.monroe@gmail.com>
2018-12-11 18:20:27 +0900

    Typo in refactoring

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
-class Api
-  class V1
+module Api
+  module V1
     class UsersController < ApplicationController
       def create
         user = UserService.new(JSON.parse(request.body.read)).call
