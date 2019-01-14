7a0d93f4620bd5dc3b793002ad423fbe0a226eb8
Samuel Monroe <spat.monroe@gmail.com>
2019-01-14 15:21:10 +0900

    Massive Gems update

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = 'f308e89c7fd9b91c27a2b3d1e901f05fd200c174f6700c34b34a56f3098bcb37736c404a288d2c6b142bf86166ab031683d47506f594f97cd1b61099c4f6124a'
+  # config.secret_key = '8ac5319f92bcaec10872cca5c50beb68e71590cca484a13f331846b35b9bd4c0c01e8df3651697ad09883009424af837fe1238f436f8123756abdffc69ac9327'
   
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -114,7 +114,7 @@ Devise.setup do |config|
   config.stretches = Rails.env.test? ? 1 : 11
 
   # Set up a pepper to generate the hashed password.
-  # config.pepper = '820e763dc8a719adb0766586fce303422991841b13155d53cd0c3bb4e5ad92d52380a2eeff3cefb41b7d6ffef22ebd4c8780488302e836c490ab81e83cb393d1'
+  # config.pepper = '7b37371e1f2d3c5d117dbd6fe5e557feffb4454b62ccb3fd60046eecffb5b5b3eb08133afd994fd3d4fed47faacc2f85b1a114ca53d554e3cfce4f56e674d383'
 
   # Send a notification to the original email when the user's email is changed.
   # config.send_email_changed_notification = false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_22_074224) do
+ActiveRecord::Schema.define(version: 2019_01_14_060700) do
 
   create_table "job_mandatory_splayds", force: :cascade do |t|
     t.integer "job_id"
