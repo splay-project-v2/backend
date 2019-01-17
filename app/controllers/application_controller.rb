5671e43bedaf1f0e65f7e6d3eea2edc9713e00b8
Samuel Monroe <spat.monroe@gmail.com>
2019-01-17 11:33:47 +0900

    Code style and cleaning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::API
   include ActiveSupport::Rescuable
 
-  rescue_from ActiveRecord::RecordInvalid do |e| render_error(e.to_s, :unprocessable_entity) end
-  rescue_from ActiveRecord::RecordNotFound do |e| render_error(e.to_s, :not_found) end
-  rescue_from AuthenticationService::Unauthorized do |e| render_error(e.to_s, :unauthorized) end
+  rescue_from(ActiveRecord::RecordInvalid) { |e| render_error(:unprocessable_entity, e.to_s) }
+  rescue_from(ActiveRecord::RecordNotFound) { |e| render_error(:not_found, e.to_s) }
+  rescue_from(AuthenticationService::Unauthorized) { |e| render_error(:unauthorized, e.to_s) }
 
   def generate_jwt(id, email)
-    JWT.encode({'id': id, 'email': email}, Rails.application.credentials.jwt_secret, 'HS256')
+    JWT.encode({ 'id': id, 'email': email }, Rails.application.credentials.jwt_secret, 'HS256')
   end
 
   private
 
-  def render_error(msg = nil, status)
-    render json: {'errors': msg}, status: status
+  def render_error(status, msg = nil)
+    render json: { 'errors': msg }, status: status
   end
 end
