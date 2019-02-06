9699b10b38ccb40ca305f6400333349f095cf2a6
Samuel Monroe <spat.monroe@gmail.com>
2019-02-06 17:23:50 +0900

    SplaydSelection model tested and covered

--- spec/models/splayd_selections/attributes_valid_spec.rb
+++ spec/models/splayd_selections/attributes_valid_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
-RSpec.describe SplaydJob, type: :model do
+RSpec.describe SplaydSelection, type: :model do
   let!(:user) { User.create(email: 'alice@foobar.com', password: 'anicepassword09', username: 'Alice') }
   let!(:splayd) { Splayd.create(user_id: user.id, key: 'key') }
   let!(:job) { Job.create(user_id: user.id, code: file_fixture('cyclon.lua').read) }
-  let!(:splayd_job) { SplaydJob.new(job_id: job.id, splayd_id: splayd.id) }
+  let!(:splayd_selection) { SplaydSelection.new(job_id: job.id, splayd_id: splayd.id) }
 
-  context 'Creation of a valid SplaydJob' do
+  context 'Creation of a valid SplaydSelection' do
     it 'is valid' do
-      expect(splayd_job).to be_valid
+      expect(splayd_selection).to be_valid
     end
 
     it 'successfully completes' do
-      assert splayd_job.save
+      assert splayd_selection.save
     end
   end
 end
