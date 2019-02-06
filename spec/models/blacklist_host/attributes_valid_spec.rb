92607a00eced8bd043342d36843a7b946ecc6737
Samuel Monroe <spat.monroe@gmail.com>
2019-02-06 16:30:47 +0900

    BlacklistHost & JobMandatorySplayd models tested and covered

---
+++ spec/models/blacklist_host/attributes_valid_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe BlacklistHost, type: :model do
+  let!(:bk) { BlacklistHost.new(host: '192.168.1.1')}
+  context 'Creation of a valid BlacklistHost' do
+    it 'is valid' do
+      expect(bk).to be_valid
+    end
+
+    it 'successfully completes' do
+      assert bk.save
+    end
+  end
+end
